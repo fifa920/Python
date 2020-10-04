0eed0bd2846ea86a337854958471eea3fdb0cb54
Daniel_Lee <rodgerslee92@gmail.com>
2020-10-05 01:24:46 +0900

    201005 알고리즘 공부

---
+++ BaekJoon/INF_binarysearch.py
@@ -0,0 +1,18 @@
+def binarysearch(S,x):
+    low = 0
+    high = len(S)
+    location = 0
+    while (low <= high and location == 0) :
+        mid = (low+high)//2
+        if S[mid] == x :
+            location = mid
+        elif x > S[mid] :
+            low = mid + 1
+        else :
+            high = mid - 1
+
+    return location
+
+S = [1, 3, 10, 20, 33, 34, 39, 41, 49]
+
+print(binarysearch(S,2))
