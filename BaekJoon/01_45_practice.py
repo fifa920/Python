6c7616cb382dea86bc46ffc404bcd9c4686b22a3
unknown <rodgerslee92@gmail.com>
2022-02-27 15:27:30 +0900

    ASUS push

--- BaekJoon/01_45_practice.py
+++ BaekJoon/01_45_practice.py
@@ -1,32 +1,91 @@
-def quick_sort(arr,start, end):
-    if start >= end:
-        return
-    pivot = start
-    left = start + 1
-    right = end
-    while left <= right:
-        while (left <= end and arr[left] <= arr[pivot]):
-            left += 1
-        while (right > start and arr[right] >= arr[pivot]):
-            right -= 1
-        if left > right:
-            arr[pivot], arr[right] = arr[right], arr[pivot]
+# # def quick_sort(arr,start, end):
+# #     if start >= end:
+# #         return
+# #     pivot = start
+# #     left = start + 1
+# #     right = end
+# #     while left <= right:
+# #         while (left <= end and arr[left] <= arr[pivot]):
+# #             left += 1
+# #         while (right > start and arr[right] >= arr[pivot]):
+# #             right -= 1
+# #         if left > right:
+# #             arr[pivot], arr[right] = arr[right], arr[pivot]
+# #         else:
+# #             arr[left], arr[right] = arr[right], arr[left]
+# #
+# # import math
+# # def is_prime_number(x):
+# #     n = 1000
+# #     arr = [True for i in range(n+1)]
+# #
+# #     for i in range(2,int(math.sqrt(x))+1):
+# #         if arr[i] == True:
+# #             j = 2
+# #             while i*j <=n:
+# #                 arr[i*j] = False
+# #                 j+=1
+# #     return arr
+# # array = is_prime_number(91)
+# # for x in range(2, 92):
+# #     if array[x] :
+# #         print(x, end=' ')
+#
+# lst = list('string')
+#
+# from collections import deque
+#
+# # N = int(input())
+# # cards = [i for i in range(1,N+1)]
+# #
+# # while len(cards) != 1:
+# #     cards.pop(0)
+# #     cards.append(cards.pop(0))
+# # print(*cards)
+#
+# # 토마토 7576
+# M,N = map(int, input().split())
+# pan = []
+# for i in range(N):
+#     pan.append(list(map(int, input().split())))
+# visited = [[0]*M for i in range(N)]
+#
+# dy = [-1, 0, 1, 0]
+# dx = [0, 1, 0, -1]
+# ripe = deque()
+#
+# def bfs(ripe):
+#     while ripe:
+#         y,x = ripe.popleft()
+#         for i in range(4):
+#             ny = y + dy[i]
+#             nx = x + dx[i]
+#             if 0<= ny < N and 0 <= nx < M and pan[ny][nx] == 0 and visited[ny][nx] == 0:
+#                 ripe.append((ny,nx))
+#                 visited[ny][nx] = 1
+#                 pan[ny][nx] = pan[y][x] + 1
+#     for x in pan:
+#         if 0 in x:
+#             return -1
+#     return max(max(pan))-1
+#
+#
+# for y in range(N):
+#     for x in range(M):
+#         if pan[y][x] == 1:
+#             ripe.append((y,x))
+#             visited[y][x] = 1
+#
+# print(bfs(ripe))
+# # 0이 존재하면 프린트 -1
+# # 최대값이 1이면 프린트 0
+
+arr = [7, 5, 9, 0, 3, 1, 6 ,2, 4, 8]
+for i in range(1,len(arr)):
+    for j in range(i,0,-1):
+        if arr[j]<arr[j-1]:
+            arr[j],arr[j-1] = arr[j-1],arr[j]
+            print(arr)
         else:
-            arr[left], arr[right] = arr[right], arr[left]
+            break
 
-import math
-def is_prime_number(x):
-    n = 1000
-    arr = [True for i in range(n+1)]
-
-    for i in range(2,int(math.sqrt(x))+1):
-        if arr[i] == True:
-            j = 2
-            while i*j <=n:
-                arr[i*j] = False
-                j+=1
-    return arr
-array = is_prime_number(91)
-for x in range(2, 92):
-    if array[x] :
-        print(x, end=' ')
--- BaekJoon/BJ_15649_N과M(1).py
+++ BaekJoon/BJ_15649_N과M(1).py
@@ -1,19 +1,33 @@
-N, M = 4,2
-numbers = list(range(1, N + 1))
+# N, M = 4,2
+# numbers = list(range(1, N + 1))
+#
+#
+# def track(depth, ans) :
+#     if depth == M:
+#         result = list(map(str, ans))
+#         print(' '.join(result))
+#         return
+#     for number in numbers:
+#         if number in ans:
+#             continue
+#         else:
+#             ans.append(number)
+#             track(depth + 1, ans)
+#             ans.pop()
+#
+#
+# track(0,[])
 
+N,M = map(int, input().split())
 
-def track(depth, ans) :
+numbers = [i for i in range(1,N+1)]
+
+def track(depth, ans):
     if depth == M:
-        result = list(map(str, ans))
-        print(' '.join(result))
-        return
+        print(*ans)
     for number in numbers:
-        if number in ans:
-            continue
-        else:
+        if number not in ans:
             ans.append(number)
-            track(depth + 1, ans)
+            track(depth+1, ans)
             ans.pop()
-
-
 track(0,[])
